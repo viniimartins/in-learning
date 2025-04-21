b4abdcf3030ef47ac05725b01aada293334827cc
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-04-20 21:07:03 -0300

    chore(api): fastify

---
+++ apps/api/src/app.ts
@@ -0,0 +1,38 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'In-Learning',
+      description: 'Full-stack e-commerce platform by Univinte.',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
---
+++ apps/api/src/server.ts
@@ -0,0 +1,12 @@
+import { env } from '@in-learning/env'
+
+import { app } from './app'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running!')
+  })
