376d2629c63d1d8ffee39a25642cb222e1eeb97b
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-04-23 13:36:23 -0300

    feat(api): course by id

--- apps/api/src/http/controllers/course/route.ts
+++ apps/api/src/http/controllers/course/route.ts
@@ -5,11 +5,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { createCourse } from './create/create-course'
 import { deleteCourse } from './delete/delete-course'
 import { getCourses } from './get/get-courses'
+import { getCourseById } from './get-by-id/get-by-id-course'
 
 export async function courseRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
+  getCourses('/courses', app)
   createCourse('/courses', app)
   deleteCourse('/courses/:courseId', app)
-  getCourses('/courses', app)
+  getCourseById('/courses/:courseId', app)
 }
