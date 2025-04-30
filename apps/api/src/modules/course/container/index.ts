8e3903f71dfb1f07aa6a495713574474a67855b7
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-04-29 21:19:46 -0300

    refactor(api): name module course completed

--- apps/api/src/modules/course/container/index.ts
+++ apps/api/src/modules/course/container/index.ts
@@ -1,9 +1,9 @@
 import {
+  COMPLETE_COURSE_REPOSITORY_TOKEN,
   CREATE_COURSE_REPOSITORY_TOKEN,
   DELETE_COURSE_REPOSITORY_TOKEN,
   ENROLL_COURSE_REPOSITORY_TOKEN,
   FIND_COURSE_BY_ID_REPOSITORY_TOKEN,
-  MARK_COURSE_AS_COMPLETED_REPOSITORY_TOKEN,
   SEARCH_COURSES_REPOSITORY_TOKEN,
 } from '@modules/course/constants'
 import { PrismaCourseRepository } from '@modules/course/infra/prisma/repositories/prisma-course-repository'
@@ -15,8 +15,8 @@ import {
 } from '@modules/course/repositories'
 import { container } from 'tsyringe'
 
+import type { ICompleteCourseRepository } from '../repositories/complete-course-repository'
 import type { IEnrollCourseRepository } from '../repositories/enroll-course-repository'
-import type { IMarkCourseAsCompletedRepository } from '../repositories/complete-course-repository'
 
 container.registerSingleton<ICreateCourseRepository>(
   CREATE_COURSE_REPOSITORY_TOKEN,
@@ -38,7 +38,7 @@ container.registerSingleton<IEnrollCourseRepository>(
   ENROLL_COURSE_REPOSITORY_TOKEN,
   PrismaCourseRepository,
 )
-container.registerSingleton<IMarkCourseAsCompletedRepository>(
-  MARK_COURSE_AS_COMPLETED_REPOSITORY_TOKEN,
+container.registerSingleton<ICompleteCourseRepository>(
+  COMPLETE_COURSE_REPOSITORY_TOKEN,
   PrismaCourseRepository,
 )
